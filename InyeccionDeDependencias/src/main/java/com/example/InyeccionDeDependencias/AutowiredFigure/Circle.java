cab2517ac0ad7ddf849e571ae4746ba8aff8d225
Jesus Chero <cheroarge96@gmail.com>
2022-07-10 23:00:39 -0500

    uso de carga de propiedades

--- InyeccionDeDependencias/src/main/java/com/example/InyeccionDeDependencias/AutowiredFigure/Circle.java
+++ InyeccionDeDependencias/src/main/java/com/example/InyeccionDeDependencias/AutowiredFigure/Circle.java
@@ -1,17 +1,17 @@
 package com.example.InyeccionDeDependencias.AutowiredFigure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Circle implements Figure {
-
+	
+	@Value("${circle.radio}")
+	final double radio=0;
+	
 	@Override
 	public double calculateArea() {
-
-		final double radio=2;
-		
 		return Math.PI*(Math.pow(radio, 2));
-		
 	}
 
 }
--- InyeccionDeDependencias/src/main/java/com/example/InyeccionDeDependencias/AutowiredFigure/Square.java
+++ InyeccionDeDependencias/src/main/java/com/example/InyeccionDeDependencias/AutowiredFigure/Square.java
@@ -1,13 +1,17 @@
 package com.example.InyeccionDeDependencias.AutowiredFigure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Square implements Figure {
-
+	
+	@Value("${square.side}")
+	double side;
+	
 	@Override
 	public double calculateArea() {
-		double side = 4;
+		
 		return side*=side;
 	}
 
